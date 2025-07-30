9c90a462fd409434c2b5212fe686de07b184a24b
BenAsama <asamaben92@gmail.com>
2025-07-30 21:38:25 +0200

    second commit changes 30 jul

---
+++ app/models/users.py
@@ -0,0 +1,23 @@
+class user:
+  user_id: int
+  first_name : str
+  last_name : str
+  other_names: str
+  account_type : id 
+  email: str #validate the email is a type of email and 
+              #also validate the email in the signup with a code if possible
+  mobile_number: int #with country code in the front end
+  gender: str
+  address: str
+  country: str 
+  state: str
+  city: str
+  nationality: str
+  occupation: str
+  date_of_birth: str
+  marital_status: str
+
+  password: str
+ 
+
+
